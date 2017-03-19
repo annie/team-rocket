9704dbf73eda0ccc4e36cfb3c59fe1e8c44d0412
Annie Zhang <azhang496@gmail.com>
2017-03-19 14:58:44 -0400

    basic keyboard movement

--- js/createScene.js
+++ js/createScene.js
@@ -25,11 +25,52 @@ function setup() {
         PIXI.loader.resources["img/kirby-hd.png"].texture
     );
     kirby.scale.set(0.05, 0.05);
-    kirby.position.set(96, 96);
+    kirby.position.set(64, 64);
     kirby.vx = 0;
     kirby.vy = 0;
-
     stage.addChild(kirby);
+
+    // capture keyboard arrow keys
+    var left = keyboard(37),
+        up = keyboard(38),
+        right = keyboard(39),
+        down = keyboard(40);
+
+    left.press = function() {
+        kirby.vx = -0.5;
+    }
+    left.release = function() {
+        if (!right.isDown) {
+            kirby.vx = 0;
+        }
+    }
+
+    right.press = function() {
+        kirby.vx = 0.5;
+    }
+    right.release = function() {
+        if (!left.isDown) {
+            kirby.vx = 0;
+        }
+    }
+
+    up.press = function() {
+        kirby.vy = -0.5;
+    }
+    up.release = function() {
+        if (!down.isDown) {
+            kirby.vy = 0;
+        }
+    }
+
+    down.press = function() {
+        kirby.vy = 0.5;
+    }
+    down.release = function() {
+        if (!up.isDown) {
+            kirby.vy = 0;
+        }
+    }
 
     // set game state
     state = play;
@@ -37,6 +78,7 @@ function setup() {
     gameLoop();
 }
 
+// continually updates the game
 function gameLoop() {
     // this function is called 60 times / second by default
     requestAnimationFrame(gameLoop);
@@ -46,9 +88,46 @@ function gameLoop() {
     renderer.render(stage);
 }
 
+// handles gameplay
 function play() {
-    kirby.vx += 0.1;
-    kirby.vy += 0.1;
     kirby.x += kirby.vx;
-    kirby.y -= kirby.vy;
+    kirby.y += kirby.vy;
+}
+
+// keyboard input helper
+function keyboard(keyCode) {
+    var key = {
+        code: keyCode,
+        isDown: false,
+        press: undefined,
+        release: undefined,
+        downHandler: downHandler,
+        upHandler: upHandler
+    }
+
+    function downHandler(event) {
+        if (event.keyCode === key.code) {
+            if (!key.isDown && key.press) {
+                key.press();
+            }
+            key.isDown = true;
+        }
+        event.preventDefault();
+    }
+
+    function upHandler(event) {
+        if (event.keyCode === key.code) {
+            if (key.isDown && key.release) {
+                key.release();
+            }
+            key.isDown = false;
+        }
+        event.preventDefault();
+    }
+
+    // attach event listeners
+    window.addEventListener("keydown", key.downHandler.bind(key), false);
+    window.addEventListener("keyup", key.upHandler.bind(key), false);
+
+    return key;
 }
