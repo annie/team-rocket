64fa09a692d77b820058de0f544a846f5088ed6d
Annie Zhang <azhang496@gmail.com>
2017-03-19 16:09:20 -0400

    simple gravity and collision handling with 1 platform

--- js/createScene.js
+++ js/createScene.js
@@ -1,5 +1,9 @@
 // set up renderer
-var renderer = PIXI.autoDetectRenderer(256, 256);
+var renderer = PIXI.autoDetectRenderer(256, 256, {
+    antialias: false, 
+    transparent: false, 
+    resolution: 2
+});
 renderer.backgroundColor = 0x62A8E5;
 // add to DOM
 document.body.appendChild(renderer.view);
@@ -16,11 +20,22 @@ function loadProgressHandler(loader, resource) {
     console.log("loading: " + resource.url);
     console.log("progress: " + loader.progress + "%");
 }
-// load into texture cache, display sprite, and call game loop
-var state, kirby;
+
+var state, kirby, box;
 
 function setup() {
     console.log("setting up");
+
+    // create platform
+    box = new PIXI.Graphics();
+    box.beginFill(0x7B4A12);
+    box.drawRect(0, 0, 50, 8);
+    box.position.set(64, 150);
+    box.endFill();
+
+    stage.addChild(box);
+
+    // create sprite
     kirby = new PIXI.Sprite(
         PIXI.loader.resources["img/kirby-hd.png"].texture
     );
@@ -28,6 +43,7 @@ function setup() {
     kirby.position.set(64, 64);
     kirby.vx = 0;
     kirby.vy = 0;
+
     stage.addChild(kirby);
 
     // capture keyboard arrow keys
@@ -36,6 +52,7 @@ function setup() {
         right = keyboard(39),
         down = keyboard(40);
 
+    // set up keyboard functions
     left.press = function() {
         kirby.vx = -0.5;
     }
@@ -92,7 +109,71 @@ function gameLoop() {
 function play() {
     kirby.x += kirby.vx;
     kirby.y += kirby.vy;
+
+    gravity();
+
+    if (detectCollision(kirby, box)) {
+        console.log("collision");
+        kirby.vy = 0;
+    }
 }
+
+function gravity() {
+    kirby.vy += 0.03;
+}
+
+function detectCollision(r1, r2) {
+    //Define the variables we'll need to calculate
+    var hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+
+    //hit will determine whether there's a collision
+    hit = false;
+
+    //Find the center points of each sprite
+    r1.centerX = r1.x + r1.width / 2;
+    r1.centerY = r1.y + r1.height / 2; 
+    r2.centerX = r2.x + r2.width / 2; 
+    r2.centerY = r2.y + r2.height / 2;
+
+    // console.log(r1.centerY);
+    console.log(r2.y);
+
+    //Find the half-widths and half-heights of each sprite
+    r1.halfWidth = r1.width / 2;
+    r1.halfHeight = r1.height / 2;
+    r2.halfWidth = r2.width / 2;
+    r2.halfHeight = r2.height / 2;
+
+    //Calculate the distance vector between the sprites
+    vx = r1.centerX - r2.centerX;
+    vy = r1.centerY - r2.centerY;
+
+    //Figure out the combined half-widths and half-heights
+    combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+    combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+
+    //Check for a collision on the x axis
+    if (Math.abs(vx) < combinedHalfWidths) {
+
+        //A collision might be occuring. Check for a collision on the y axis
+        if (Math.abs(vy) < combinedHalfHeights) {
+
+          //There's definitely a collision happening
+          hit = true;
+        } else {
+
+          //There's no collision on the y axis
+          hit = false;
+        }
+    } else {
+
+        //There's no collision on the x axis
+        hit = false;
+    }
+
+    //`hit` will be either `true` or `false`
+    return hit;
+};
 
 // keyboard input helper
 function keyboard(keyCode) {
--- test/sample.js
+++
@@ -1,8 +0,0 @@
-var assert = require("assert");
-var THREE = require("three");
-
-describe("this is a test", function() {
-    it("1 should equal 1", function() {
-        assert.equal(1, 1);
-    })
-})
