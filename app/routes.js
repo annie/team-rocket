e69ba530ee1221cee1e680890251653ed2d0c153
Annie Zhang <azhang496@gmail.com>
2017-04-06 23:26:55 -0400

    request pkg error

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,21 @@ var User = require("./models/User");
 module.exports = function(app) {
 
     app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public", "index.html"));
+
+        var newUser = new User({
+            userId: "23875840",
+            highScore: 0
+        });
+
+        newUser.save(function (err, newUser, numAffected) {
+            if (err) {
+                console.log("there was an error");
+            }
+            console.log("newUser: " + newUser.userId);
+            console.log("numAffected: " + numAffected);
+        });
+
+        res.sendFile(path.join(__dirname, "../public", "kirby.html"));
     });
 
     app.post("/", function (req, res) {
